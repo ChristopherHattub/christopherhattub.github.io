78faddb29f1f2f0a3a6aa62f42d779a286a26896
Chris Hattub <yourusername@users.noreply.github.com>
2025-07-29 13:52:32 -0400

    Fix project images by importing directly into webpack bundle

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ProjectCarousel from '../../components/ProjectCarousel/ProjectCarousel';
 import './Projects.css';
+import constructionImage from '../../assets/construction.png';
 
 const Projects = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -20,33 +21,33 @@ const Projects = () => {
       id: 1,
       title: "IdeaBall",
       description: "application for organizing lists of ideas",
-      image: `${process.env.PUBLIC_URL}/assets/project-images/construction.png`
+      image: constructionImage
     },
     {
       id: 2,
       title: "TodoTracker",
       description: "Custom todo list app that allows users to track their progress and goals",
-      image: `${process.env.PUBLIC_URL}/assets/project-images/construction.png`
+      image: constructionImage
     },
     {
       id: 3,
       title: "Gas and Oil Spending Prediction Model",
       description: "Predicting gas and oil spending based on historical data and weather data",
-      image: `${process.env.PUBLIC_URL}/assets/project-images/construction.png`
+      image: constructionImage
     },
 
     {
       id: 4,
       title: "Project 4",
       description: "Placeholder",
-      image: `${process.env.PUBLIC_URL}/assets/project-images/construction.png`
+      image: constructionImage
     },
 
     {
       id: 5,
       title: "Project 5",
       description: "placeholder",
-      image: `${process.env.PUBLIC_URL}/assets/project-images/construction.png`
+      image: constructionImage
     },
     
     // Add more projects as needed
